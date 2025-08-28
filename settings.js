13e12b7fafe720f3415048caca632aa1d47ba62e
GoodControl <goodcontrol254@gmail.com>
2025-08-28 18:07:55 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO~21a626e8bd3450c6cd3a577747c59114b7ffa491", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "254799233766", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
